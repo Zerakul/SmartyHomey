4c38c22624c3eab3be360442d1bcc5450c711151
zerakul <seiranjaninyan@gmail.com>
2017-07-17 15:18:22 +0300

    added FAB added controller

--- app/src/main/java/com/homey/smarty/smartyhomey/MainActivity.java
+++ app/src/main/java/com/homey/smarty/smartyhomey/MainActivity.java
@@ -1,15 +1,44 @@
 package com.homey.smarty.smartyhomey;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
+
+
+
+public class MainActivity extends FragmentActivity {
 
     //Test
-
+    public FABToolbarLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setFab();
+    }
+
+    private void setFab() {
+        View fab;
+        layout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+        fab = findViewById(R.id.fabtoolbar_fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.show();
+            }
+        });
+        layout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                layout.hide();
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/homey/smarty/smartyhomey/Managers/Controller.java
@@ -0,0 +1,8 @@
+package com.homey.smarty.smartyhomey.Managers;
+
+/**
+ * Created by Seiran on 17/07/2017.
+ */
+
+public class Controller {
+}

150295f1ff993ac8e43f556a96c61b2bf7531533
zerakul <seiranjaninyan@gmail.com>
2017-07-17 15:37:45 +0300

    Running Version

--- app/src/main/java/com/homey/smarty/smartyhomey/Managers/Controller.java
+++ app/src/main/java/com/homey/smarty/smartyhomey/Managers/Controller.java
@@ -5,4 +5,17 @@ package com.homey.smarty.smartyhomey.Managers;
  */
 
 public class Controller {
+
+
+    public static boolean VerifyUser(String name, String password){
+         if(name.equals("a@a.com"))
+             if(password.equals("a"))
+                 return true;
+
+        return false;
+
+
+
+
+    }
 }
